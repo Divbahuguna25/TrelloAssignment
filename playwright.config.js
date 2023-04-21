1a24d63ce0efa841c600741e8de22eda610ff1e3
Divyansh Bahuguna <divyansh.bahuguna@yocket.in>
2023-04-21 14:02:21 +0530

    adding viewport

--- playwright.config.js
+++ playwright.config.js
@@ -40,7 +40,10 @@ module.exports = defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1536, height: 864 },
+      },
     },
 
     // {
